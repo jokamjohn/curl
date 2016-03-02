f6b8028f15b6c8ee1bb94d0a357a6e52178a6efe
johnkagga <johnkagga@gmail.com>
2016-03-02 20:29:27 +0300

    Error handling

--- google.php
+++ google.php
@@ -12,9 +12,9 @@ const GOOGLE_URL = "http://www.google.com/search?q=curl";
 $curl = curl_init();
 
 //Step 2: set some options.
-curl_setopt($curl,CURLOPT_URL,GOOGLE_URL);
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($curl,CURLOPT_HEADER,false);
+curl_setopt($curl, CURLOPT_URL, GOOGLE_URL);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
 
 //Step 3: Get the result.
 $result = curl_exec($curl);
@@ -22,4 +22,10 @@ $result = curl_exec($curl);
 //Step 4: close curl.
 curl_close($curl);
 
-echo $result;
+//Step 5: error handlin.
+
+if ($result == false) {
+    echo "Curl error: " . curl_error($curl);
+} else {
+    echo $result;
+}
